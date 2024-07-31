66c9c08ecaa945b74d09e1b71133f4dd97a478cc
Robin Reckmann <robin.reckmann@gmail.com>
2024-08-01 02:50:17 +0900

    dump of modifications to original pyenvtouv

--- userspace/openpilot_python_dependencies.sh
+++ userspace/openpilot_python_dependencies.sh
@@ -3,22 +3,11 @@
 echo "Installing python for openpilot"
 
 echo "installing uv..."
-curl -LsSf https://astral.sh/uv/install.sh | sh
-UV_BIN='$HOME/.cargo/env'
-ADD_PATH_CMD=". \"$UV_BIN\""
-eval $ADD_PATH_CMD
+export XDG_DATA_HOME="/usr/local"
+export CARGO_HOME="$XDG_DATA_HOME/.cargo"
 
-PYTHON_VERSION="3.11.4"
-if [ "$(uname -p)" == "aarch64" ]; then
-  uv python install $PYTHON_VERSION
-else
-  MAKEFLAGS="-j1" MAKE_OPTS="-j1" taskset --cpu-list 0 uv python install --verbose $PYTHON_VERSION
-fi
+curl -LsSf https://astral.sh/uv/install.sh | sh
+eval ". $CARGO_HOME/env"
 
 # uv requires virtual env either managed or system before installing dependencies
-uv venv --python-preference only-managed
-# need to activate virtual env otherwise call to uv pip install throws error,
-# error: No virtual or system environment found for path ...
-source .venv/bin/activate
-# install dependencies using managed python
-uv pip install --python=$(which python) --no-cache-dir --upgrade pip
+uv venv $XDG_DATA_HOME/venv --python-preference only-managed --python=3.11.4
