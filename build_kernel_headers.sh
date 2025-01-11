7680e2a38583b07b54bda6f5a93ac54eb12cd3d6
Robin Reckmann <robin.reckmann@gmail.com>
2025-01-11 17:49:20 +0900

    Add scripts for kernel header building

--- build_kernel_headers.sh
+++ build_kernel_headers.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-DEFCONFIG=tici_defconfig
+DEFCONFIG=defconfig
 
 # Get directories and make sure we're in the correct spot to start the build
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
@@ -11,34 +11,71 @@ OUTPUT_DIR=$DIR/output
 BOOT_IMG=./boot.img
 cd $DIR
 
-# Clone kernel if not done already
-if [ ! -d agnos-kernel-sdm845 ]; then
-  git submodule init agnos-kernel-sdm845
-fi
-cd agnos-kernel-sdm845
-
-# Build parameters
-export ARCH=arm64
-if [ ! -f /TICI ]; then
-  export CROSS_COMPILE=$TOOLS/aarch64-linux-gnu-gcc/bin/aarch64-linux-gnu-
-  export CC=$TOOLS/aarch64-linux-gnu-gcc/bin/aarch64-linux-gnu-gcc
-  export LD=$TOOLS/aarch64-linux-gnu-gcc/bin/aarch64-linux-gnu-ld.bfd
+if [[ "$(uname)" == 'Darwin' ]]; then
+  BASE_VOLUME_PATH=$(echo $DIR | grep -o "^/Volumes/[^/]*" || echo "/")
+  if ! diskutil info -plist $BASE_VOLUME_PATH | grep -q "<string>Case-sensitive APFS</string>"; then
+    echo "---------------   macOS support   ---------------"
+    echo "Ensure you are in a Case-sensitive APFS volume to build the AGNOS kernel."
+    echo "https://github.com/commaai/agnos-builder?tab=readme-ov-file#macos"
+    echo "-------------------------------------------------"
+    exit 1
+  fi
 fi
 
-# these do anything?
-export KCFLAGS="-w"
+# Setup kernel build container
+echo "Building agnos-meta-builder docker image"
+export DOCKER_BUILDKIT=1
+docker build -f Dockerfile.builder -t agnos-meta-builder $DIR \
+  --build-arg UNAME=$(id -nu) \
+  --build-arg UID=$(id -u) \
+  --build-arg GID=$(id -g)
+echo "Starting agnos-meta-builder container"
+CONTAINER_ID=$(docker run -d -v $DIR:$DIR -w $DIR agnos-meta-builder)
 
-# Load defconfig and build kernel
-echo "-- First make --"
-make $DEFCONFIG O=out
-echo "-- Second make: $(nproc --all) cores --"
-ARGS=""
-if [ -f /TICI ]; then
-  ARGS="sudo -E"
-fi
-$ARGS make bindeb-pkg -j$(nproc --all) O=out  # Image.gz-dtb
+# Cleanup container on exit
+trap "echo \"Cleaning up container:\"; \
+docker container rm -f $CONTAINER_ID" EXIT
 
-# Copy output
-mkdir -p $OUTPUT_DIR
-cp linux-headers-*.deb $OUTPUT_DIR
+$DIR/tools/extract_tools.sh
 
+build_kernel() {
+  cd linux-kernel
+
+  # Build parameters
+  ARCH=$(uname -m)
+  if [ "$ARCH" != "arm64" ] && [ "$ARCH" != "aarch64" ]; then
+    export CROSS_COMPILE=$TOOLS/aarch64-linux-gnu-gcc/bin/aarch64-linux-gnu-
+    export CC=$TOOLS/aarch64-linux-gnu-gcc/bin/aarch64-linux-gnu-gcc
+    export LD=$TOOLS/aarch64-linux-gnu-gcc/bin/aarch64-linux-gnu-ld.bfd
+  fi
+
+  # Build arm64 arch
+  export ARCH=arm64
+
+  # Set ccache dir
+  export CCACHE_DIR=$DIR/.ccache
+
+  # Avoid LINUX_COMPILE_HOST to change on every run thus invalidating cache
+  # https://patchwork.kernel.org/project/linux-kbuild/patch/1302015561-21047-8-git-send-email-mmarek@suse.cz/
+  export KBUILD_BUILD_HOST="docker"
+
+  # Disable all warnings
+  export KCFLAGS="-w"
+
+  # Remove old deb files
+  rm linux-*.deb
+  rm linux-*.buildinfo
+  rm linux-*.changes
+
+  # Load defconfig and build kernel
+  echo "-- Config --"
+  make $DEFCONFIG comma3.config O=out
+  echo "-- Make: $(nproc --all) cores --"
+  make bindeb-pkg -j$(nproc --all) O=out
+
+  # Copy to output dir
+  mkdir -p $OUTPUT_DIR
+  cp linux-*.deb $OUTPUT_DIR/
+}
+# Run build_kernel in container
+docker exec -u $(id -nu) $CONTAINER_ID bash -c "set -e; export DEFCONFIG=$DEFCONFIG DIR=$DIR TOOLS=$TOOLS TMP_DIR=$TMP_DIR OUTPUT_DIR=$OUTPUT_DIR BOOT_IMG=$BOOT_IMG; $(declare -f build_kernel); build_kernel"
